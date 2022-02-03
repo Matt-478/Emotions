3bc410d48be850c402626ef3c41844d8ea0545a7
Matīss Komļevs <matisskomlevs@gmail.com>
2022-02-03 22:07:00 +0200

    Installed and created a bg colour scroll package

--- emotions-app/src/App.js
+++ emotions-app/src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ScrollingColorBackground from 'react-scrolling-color-background'
+
 function App() {
   return (
-    <div className="App">
-          All the basic emotions...
-    </div>
+    <>
+      <p>
+        All the basic emotions...
+      </p>
+      <div>
+        <ScrollingColorBackground 
+         selector='.js-color-stop[data-background-color]'
+         colorDataAttribute='data-background-color'
+         initialRgb='rgb(0, 0, 0)'/>
+         <section data-background-color='rgb(102, 119, 97)' className='js-color-stop' />
+         <section data-background-color='rgb(84, 94, 86)' className='js-color-stop' />
+         <section data-background-color="rgb(145, 124, 120)" className='js-color-stop'></section>
+         <section data-background-color="rgb(183, 148, 146)" className="js-color-stop"></section>
+      </div>
+
+      <p>YEAP</p>
+    </>
   );
 }
 
