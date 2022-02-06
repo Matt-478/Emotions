9ebf55310ab1cd3bbf92824ad82e39d1346641cf
Matīss Komļevs <matisskomlevs@gmail.com>
2022-02-06 15:07:05 +0200

    starting from scratch
    removed dependency for bg - didn't work

--- emotions-app/src/App.js
+++ emotions-app/src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
 import React from 'react'
-import Boops from './components/Boops'
-import Background from './components/Background'
-
-import ScrollingColorBackground from 'react-scrolling-color-background'
 
 function App() {
   return (
@@ -11,23 +7,6 @@ function App() {
       <p>
         All the basic emotions...
       </p>
-      {/* <Background /> */}
-      <Boops />
-      <ScrollingColorBackground
-        selector='.js-color-stop[data-background-color]'
-        colorDataAttribute='data-background-color'
-        initialRgb='rgb(0, 0, 0)'
-      />
-      ...
-      <section
-        data-background-color='rgb(32, 202, 172)'
-        className='js-color-stop'
-      >Some content</section>
-      ...
-      <section
-        data-background-color='rgb(60, 191, 246)'
-        className='js-color-stop'
-      />Some other content
     </>
   );
 }
