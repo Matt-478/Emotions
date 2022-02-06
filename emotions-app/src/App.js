007ee67dcb48b827ee0abb91cf1acb4b4f6b8e16
Matīss Komļevs <matisskomlevs@gmail.com>
2022-02-06 15:45:08 +0200

    basic html structure setup

--- emotions-app/src/App.js
+++ emotions-app/src/App.js
@@ -4,9 +4,24 @@ import React from 'react'
 function App() {
   return (
     <>
-      <p>
-        All the basic emotions...
-      </p>
+      <main>
+        <section className="intro">
+          <h3>Welcome to Emotions</h3>
+          <p>All the basic emotions...</p>
+        </section>
+
+        <section className="emotion_1">
+          <h3>Anger</h3>
+        </section>
+
+        <section className="emotion_2">
+          <h3>Enjoyment</h3>
+        </section>
+
+        <section className="emotion_3">
+          <h3>Fear</h3>
+        </section>
+      </main>
     </>
   );
 }
