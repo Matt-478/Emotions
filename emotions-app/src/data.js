75cd44ee8e44df57005c3bed08c356ba1e722237
Matīss Komļevs <matisskomlevs@gmail.com>
2022-02-09 22:15:14 +0200

    installed font for fear, added mixin for font change on hover

--- emotions-app/src/data.js
+++ emotions-app/src/data.js
@@ -10,6 +10,21 @@ export const data = [
     classNameForFontChange: "angerFontToChange"
   },
   {
+    title: "Sadness",
+    background: "rgb(230, 210, 99)",
+    classNameForFontChange: ""
+  },
+  {
+    title: "Surprise",
+    background: "rgb(209, 85, 47)",
+    classNameForFontChange: "angerFontToChange"
+  },
+  {
+    title: "Rejection",
+    background: "rgb(230, 210, 99)",
+    classNameForFontChange: ""
+  },
+  {
     title: "Enjoyment",
     background: "rgb(230, 210, 99)",
     classNameForFontChange: ""
@@ -17,6 +32,21 @@ export const data = [
   {
     title: "Fear",
     background: "rgb(112, 122, 139)",
+    classNameForFontChange: "fearFontToChange"
+  },
+  {
+    title: "Disgust",
+    background: "rgb(112, 122, 139)",
     classNameForFontChange: ""
-  }
+  },
+  // {
+  //   title: "Love",
+  //   background: "rgb(112, 122, 139)",
+  //   classNameForFontChange: ""
+  // },
+  // {
+  //   title: "Shame",
+  //   background: "rgb(112, 122, 139)",
+  //   classNameForFontChange: ""
+  // },
 ]
