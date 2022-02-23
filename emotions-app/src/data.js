a8900060c78a69cbff5a1306f1ebcb2a561c622d
Matīss Komļevs <matisskomlevs@gmail.com>
2022-02-23 15:42:24 +0200

    commented out some data for a bit

--- emotions-app/src/data.js
+++ emotions-app/src/data.js
@@ -5,13 +5,13 @@ export const data = [
     background: "cornflowerblue",
     classNameForFontChange: ""
   },
-  {
-    title: "Anger",
-    text_description: "  Anger can be a particularly powerful emotion characterized by feelings of hostility, agitation, frustration, and antagonism towards others. Like fear, anger can play a part in your body's fight or flight response.  When a threat generates feelings of anger, you may be inclined to fend off the danger and protect yourself. Anger is often displayed through: Facial expressions: such as frowning or glaring. Body language: such as taking a strong stance or turning away. Tone of voice: such as speaking gruffly or yelling. Physiological responses: such as sweating or turning red. Aggressive behaviors: such as hitting, kicking, or throwing objects. While anger is often thought of as a negative emotion, it can sometimes be a good thing. It can be constructive in helping clarify your needs in a relationship, and it can also motivate you to take action and find solutions to things that are bothering you.",
-    background: "rgb(209, 85, 47)",
-    classNameForFontChange: "angerFontToChange",
-    realFontFamily: "Aarcover"
-  },
+  // {
+  //   title: "Anger",
+  //   text_description: "  Anger can be a particularly powerful emotion characterized by feelings of hostility, agitation, frustration, and antagonism towards others. Like fear, anger can play a part in your body's fight or flight response.  When a threat generates feelings of anger, you may be inclined to fend off the danger and protect yourself. Anger is often displayed through: Facial expressions: such as frowning or glaring. Body language: such as taking a strong stance or turning away. Tone of voice: such as speaking gruffly or yelling. Physiological responses: such as sweating or turning red. Aggressive behaviors: such as hitting, kicking, or throwing objects. While anger is often thought of as a negative emotion, it can sometimes be a good thing. It can be constructive in helping clarify your needs in a relationship, and it can also motivate you to take action and find solutions to things that are bothering you.",
+  //   background: "rgb(209, 85, 47)",
+  //   classNameForFontChange: "angerFontToChange",
+  //   realFontFamily: "Aarcover"
+  // },
   {
     title: "Sadness",
     text_description: "Sadness is another type of emotion often defined as a transient emotional state characterized by feelings of disappointment, grief, hopelessness, disinterest, and dampened mood. Like other emotions, sadness is something that all people experience from time to time. In some cases, people can experience prolonged and severe periods of sadness that can turn into depression. Sadness can be expressed in a number of ways including: Crying, Dampened mood, Lethargy, Quietness, Withdrawal from others. The type and severity of sadness can vary depending upon the root cause, and how people cope with such feelings can also differ",
@@ -19,13 +19,13 @@ export const data = [
     classNameForFontChange: "sadnessFontToChange",
     realFontFamily: "Rof Top Simple"
   },
-  {
-    title: "Surprise",
-    text_description: "Surprise is another one of the six basic types of human emotions originally described by Eckman. Surprise is usually quite brief and is characterized by a physiological startle response following something unexpected. This type of emotion can be positive, negative, or neutral. An unpleasant surprise, for example, might involve someone jumping out from behind a tree and scaring you as you walk to your car at night. An example of a pleasant surprise would be arriving home to find that your closest friends have gathered to celebrate your birthday. Surprise is often characterized by: Facial expressions: such as raising the brows, widening the eyes, and opening the mouth. Physical responses: such as jumping back. Verbal reactions: such as yelling, screaming, or gasping. Surprise is another type of emotion that can trigger the fight or flight response.",
-    background: "rgb(209, 85, 47)",
-    classNameForFontChange: "surpriseFontToChange",
-    realFontFamily: "Fresnel"
-  },
+  // {
+  //   title: "Surprise",
+  //   text_description: "Surprise is another one of the six basic types of human emotions originally described by Eckman. Surprise is usually quite brief and is characterized by a physiological startle response following something unexpected. This type of emotion can be positive, negative, or neutral. An unpleasant surprise, for example, might involve someone jumping out from behind a tree and scaring you as you walk to your car at night. An example of a pleasant surprise would be arriving home to find that your closest friends have gathered to celebrate your birthday. Surprise is often characterized by: Facial expressions: such as raising the brows, widening the eyes, and opening the mouth. Physical responses: such as jumping back. Verbal reactions: such as yelling, screaming, or gasping. Surprise is another type of emotion that can trigger the fight or flight response.",
+  //   background: "rgb(209, 85, 47)",
+  //   classNameForFontChange: "surpriseFontToChange",
+  //   realFontFamily: "Fresnel"
+  // },
   {
     title: "Enjoyment",
     text_description: "Enjoyment also known as happiness is often defined as a pleasant emotional state that is characterized by feelings of contentment, joy, gratification, satisfaction, and well-being. Research on happiness has increased significantly since the 1960s within a number of disciplines, including the branch of psychology known as positive psychology. This type of emotion is sometimes expressed through: Facial expressions: such as smiling. While happiness is considered one of the basic human emotions, the things we think will create happiness tend to be heavily influenced by culture. For example, pop culture influences tend to emphasize that attaining certain things such as buying a home or having a high-paying job will result in happiness. The realities of what actually contributes to happiness are often much more complex and more highly individualized.",
@@ -40,11 +40,11 @@ export const data = [
     classNameForFontChange: "fearFontToChange",
     realFontFamily: "Tupo Vyaz"
   },
-  {
-    title: "Disgust",
-    text_description: "Disgust is another of the original six basic emotions described by Eckman. Disgust can be displayed in a number of ways including: Body language: turning away from the object of disgust. Physical reactions: such as vomiting or retching. Facial expressions: such as wrinkling the nose and curling the upper lip. This sense of revulsion can originate from a number of things, including an unpleasant taste, sight, or smell. Researchers believe that this emotion evolved as a reaction to foods that might be harmful or fatal. When people smell or taste foods that have gone bad, for example, disgust is a typical reaction. Poor hygiene, infection, blood, rot, and death can also trigger a disgust response. This may be the body's way of avoiding things that may carry transmittable diseases.",
-    background: "rgb(112, 122, 139)",
-    classNameForFontChange: "disgustFontToChange",
-    realFontFamily: "UnifrakturMaguntia"
-  },
+  // {
+  //   title: "Disgust",
+  //   text_description: "Disgust is another of the original six basic emotions described by Eckman. Disgust can be displayed in a number of ways including: Body language: turning away from the object of disgust. Physical reactions: such as vomiting or retching. Facial expressions: such as wrinkling the nose and curling the upper lip. This sense of revulsion can originate from a number of things, including an unpleasant taste, sight, or smell. Researchers believe that this emotion evolved as a reaction to foods that might be harmful or fatal. When people smell or taste foods that have gone bad, for example, disgust is a typical reaction. Poor hygiene, infection, blood, rot, and death can also trigger a disgust response. This may be the body's way of avoiding things that may carry transmittable diseases.",
+  //   background: "rgb(112, 122, 139)",
+  //   classNameForFontChange: "disgustFontToChange",
+  //   realFontFamily: "UnifrakturMaguntia"
+  // },
 ]
