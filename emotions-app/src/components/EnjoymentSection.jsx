ed02d4a4fc802572da74c6ff850f024abab37b0c
Matīss Komļevs <matisskomlevs@gmail.com>
2022-02-28 17:55:07 +0200

    converted to functional component

--- emotions-app/src/components/EnjoymentSection.jsx
+++ emotions-app/src/components/EnjoymentSection.jsx
@@ -1,33 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { data } from '../data'
 import DemoBtn from './DemoBtn'
 
-export default class EnjoymentSection extends Component {
-  // constructor(props){
-  //   super(props)
-  // }
+export default function EnjoymentSection ({ demoToggle, changeState }) {
 
-  render() {
-    let enjoymentObj = this.props.data.find( ({title}) => {
-      if (title === "Enjoyment") {
-        return title
-      }
-    })
-    console.log(enjoymentObj)
+  let enjoymentObj = data.find((obj) => {
+    if(obj.title === "Enjoyment") {
+      return obj.emotionProps
+    }
+  })
+  console.log(enjoymentObj)
+
     return (
-      // <div>
+      <>
         <section 
-          // ref={(el) => (GroupRef.current[i] = el)}
-          // data-bgcolor={enjoymentObj.background}
-          // id={enjoymentObj.title}
           className={"p-relative"}
-          ref={this.props.ref}
-
-          data-bgcolor={this.props.data_bgcolor}
-          id={this.props.id}
+          id={enjoymentObj.title}
           >
             <h1 
             className={"allFontsProps emotionTitle"}
-            style={{fontFamily: this.props.demoToggle ? enjoymentObj.realFontFamily : 'Roboto Condensed'}}
+            style={{fontFamily: demoToggle ? enjoymentObj.emotionProps.realFontFamily : 'Roboto Condensed'}}
             >
               {enjoymentObj.title}
             </h1>
@@ -36,19 +28,17 @@ export default class EnjoymentSection extends Component {
               width: "30em",
               position: "absolute",
               top: "8em"}}>
-            {enjoymentObj.text_description}
+            {enjoymentObj.emotionProps.text_description}
           </p>
           <DemoBtn
-           toggleValue={this.props.demoToggle}
-           setToggleValue={this.props.changeState}
+           toggleValue={demoToggle}
+           setToggleValue={changeState}
            style={{
             position: "absolute",
             right: "0",
             bottom: "5em"}}
            />
         </section>
-        
-      /* </div> */
+      </>
     )
   }
-}
