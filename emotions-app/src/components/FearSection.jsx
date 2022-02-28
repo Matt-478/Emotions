b28c810aa4c4196dd4f5763b0419b2643bea81ec
Matīss Komļevs <matisskomlevs@gmail.com>
2022-02-28 17:40:02 +0200

    rewritten to a functional component

--- emotions-app/src/components/FearSection.jsx
+++ emotions-app/src/components/FearSection.jsx
@@ -1,46 +1,46 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { data } from '../data'
 
-export default class FearSection extends Component {
-  render() {
-    let fearObj = data.find((obj) => {
-      if(obj.title === "Fear") {
-        return obj.emotionProps
-      }
-    })
-    console.log(fearObj)
-    return (
-      <>
-      {/* <section 
-          className={"p-relative"}
-          ref={this.props.ref}
+export default function FearSection() {
 
-          data-bgcolor={this.props.data_bgcolor}
-          id={this.props.id}
+  let fearObj = data.find((obj) => {
+    if(obj.title === "Fear") {
+      return obj.emotionProps
+    }
+  })
+  console.log(fearObj)
+
+  return (
+    <>
+    {/* <section 
+        className={"p-relative"}
+        ref={this.props.ref}
+
+        data-bgcolor={this.props.data_bgcolor}
+        id={this.props.id}
+        >
+          <h1 
+          className={"allFontsProps emotionTitle"}
+          style={{fontFamily: this.props.demoToggle ? enjoymentObj.realFontFamily : 'Roboto Condensed'}}
           >
-            <h1 
-            className={"allFontsProps emotionTitle"}
-            style={{fontFamily: this.props.demoToggle ? enjoymentObj.realFontFamily : 'Roboto Condensed'}}
-            >
-              {enjoymentObj.title}
-            </h1> */}
-          <p
-            style={{
-              width: "30em",
-              position: "absolute",
-              top: "8em"}}>
-            {/* {enjoymentObj.text_description} */}
-          </p>
-          {/* <DemoBtn
-           toggleValue={this.props.demoToggle}
-           setToggleValue={this.props.changeState}
-           style={{
+            {enjoymentObj.title}
+          </h1> */}
+        <p
+          style={{
+            width: "30em",
             position: "absolute",
-            right: "0",
-            bottom: "5em"}}
-           /> */}
-        {/* </section> */}
-      </>
-    )
-  }
+            top: "8em"}}>
+          {/* {enjoymentObj.text_description} */}
+        </p>
+        {/* <DemoBtn
+         toggleValue={this.props.demoToggle}
+         setToggleValue={this.props.changeState}
+         style={{
+          position: "absolute",
+          right: "0",
+          bottom: "5em"}}
+         /> */}
+      {/* </section> */}
+    </>
+  )
 }
