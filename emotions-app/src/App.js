ffa04521726b5b63457544cf43ed6758d1b5fd15
Matīss Komļevs <matisskomlevs@gmail.com>
2022-03-11 17:31:17 +0200

    created a function to access component id
    Helps with another function to check on which component we press and hence can also start/fire animations for each part individually and not all at once

--- emotions-app/src/App.js
+++ emotions-app/src/App.js
@@ -11,14 +11,31 @@ import FearSection from './components/FearSection'
 
 function App() {
   const[demoToggle, setDemoToggle] = useState(false)
+  useEffect(() => {
+    createEmotionIds()
+  }, [])
+
+  function createEmotionIds () {
+    // Access the elements
+    // loop through all the children of "main"
+    // return new variables, each with their according id value
+    let mainNode = document.getElementsByTagName("main")
+    let arrayOfChildNodes = mainNode[0].childNodes
+    let arrayofChildElements = Array.from(arrayOfChildNodes)
+    console.log(arrayofChildElements)
+
+    arrayofChildElements.forEach((item) => {
+      console.log("id values " , item.id)
+    })
+  }
 
   return (
     <>
       <main>
         <WelcomeSection demoToggle={demoToggle}/>
-        <SadnessSection demoToggle={demoToggle} changeState={setDemoToggle}/>
-        <FearSection demoToggle={demoToggle} changeState={setDemoToggle}/>
-        <EnjoymentSection demoToggle={demoToggle} changeState={setDemoToggle}/>
+        <SadnessSection id={"Sadness"} demoToggle={demoToggle} changeState={setDemoToggle}/>
+        <FearSection id={"Fear"} demoToggle={demoToggle} changeState={setDemoToggle}/>
+        <EnjoymentSection id={"Enjoyment"} demoToggle={demoToggle} changeState={setDemoToggle}/>
       </main>
     </>
   );
